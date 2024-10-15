ed1439f3f9a302ceb033fdfcc31a421611029661
Greg <6regmcc@gmail.com>
2024-10-15 15:48:23 +0100

    added create user test

--- tests/integration_tests/test_user_database.py
+++ tests/integration_tests/test_user_database.py
@@ -1,7 +1,10 @@
+import pydantic_core
 import pytest
 import sqlalchemy
+from pydantic import ValidationError
 
-from db.db_user_auth import db_get_user_by_username, db_get_user_details_by_id
+from db.db_user_auth import db_get_user_by_username, db_get_user_details_by_id, db_create_user
+from schemas.user_schema import Create_User, Return_User
 
 
 # db_get_user_by_username
@@ -27,8 +30,23 @@ def test_get_user_details_by_id_failure(db_session):
         db_get_user_details_by_id(user_auth_id=9999999999, db=db_session)
 
 
-
-
 def test_db_create_user(db_session):
-    pass
-# db_create_user
+    new_user = Create_User(
+        username="tom_smith@email.com",
+        password="Abc123",
+        first_name="Tom",
+        last_name="Smith"
+    )
+    new_user = db_create_user(create_user_data=new_user, db=db_session)
+    assert isinstance(new_user, Return_User)
+
+
+def test_db_create_user_duplicate(create_user_fixture, db_session):
+    new_user = create_user_fixture
+    new_duplicate_user = Create_User(
+        username=new_user["username"],
+        password="Password1"
+    )
+    with pytest.raises(sqlalchemy.exc.IntegrityError):
+
+        db_create_user(create_user_data=new_duplicate_user, db=db_session)
