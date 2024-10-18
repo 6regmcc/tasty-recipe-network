92d6d79f7a704271cb2c15cba791b0191ac432bf
Greg <6regmcc@gmail.com>
2024-10-18 21:12:47 +0100

    added create_recipe route

--- app/routes/recipe_routes.py
+++ app/routes/recipe_routes.py
@@ -6,6 +6,7 @@ from starlette.requests import Request
 
 from authentication.user_auth_routes import oauth2_scheme, router, get_current_user
 from db.db_connection import get_db
+from db.db_recipes import db_create_recipe_with_ingredients
 from schemas.recipe_schema import Create_Recipe
 
 recipe_router = APIRouter(
@@ -16,13 +17,9 @@ recipe_router = APIRouter(
 
 
 @recipe_router.post("/create_recipe")
-def create_recipe(new_recipe: Create_Recipe, db: Annotated[Session, Depends(get_db)], token: Annotated[str, Depends(oauth2_scheme)]):
+def create_recipe(recipe_data: Create_Recipe, db: Annotated[Session, Depends(get_db)], token: Annotated[str, Depends(oauth2_scheme)]):
     user_id = get_current_user(token=token, db=db).user_id
+    new_recipe = db_create_recipe_with_ingredients(recipe_data=recipe_data, user_id=user_id, db=db)
+    return new_recipe
 
-    return token
 
-
-@recipe_router.get("/test")
-def test_stuff(db: Annotated[Session, Depends(get_db)], token: Annotated[str, Depends(oauth2_scheme)]):
-
-    return user_id
