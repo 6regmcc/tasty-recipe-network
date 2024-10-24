b1d62ba80b12eef76f6fbb4a0dd752a42408336f
Greg McCarthy <6regmcc@gmail.com>
2024-10-24 09:16:42 +0100

    add dockercompose with dev_db

--- tasty_recipe_network/config.py
+++ tasty_recipe_network/config.py
@@ -3,5 +3,5 @@
 ALGORITHM="HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES="300"
 
-DEV_DATABASE_URL="postgresql+psycopg://postgres:postgres@docker.for.mac.localhost/tasty_dev_db"
+DEV_DATABASE_URL="postgresql+psycopg://postgres:postgres@dev_db:5432/tasty_dev_db"
 TEST_DATABASE_URL="postgresql+psycopg://postgres:postgres@docker.for.mac.localhost:5432/tasty_test_db"
