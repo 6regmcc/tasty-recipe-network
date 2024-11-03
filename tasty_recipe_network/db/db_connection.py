44f6297ee53b88d845b82eecf811a11f33e8ceef
Greg McCarthy <6regmcc@gmail.com>
2024-11-03 13:40:02 +0000

    completed mypy refactoring on db_connections

--- tasty_recipe_network/db/db_connection.py
+++ tasty_recipe_network/db/db_connection.py
@@ -2,22 +2,33 @@ import os
 from typing import Generator
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import Session, sessionmaker, DeclarativeBase
 from typing import TYPE_CHECKING
 # from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import declarative_base
 
 from tasty_recipe_network.config import DEV_DATABASE_URL
 
-DATABASE_URL = ""
-environment = os.environ.get('ENVIRONMENT')
 
-if environment == "production":
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-else:
-    DATABASE_URL = DEV_DATABASE_URL
+def get_environment() -> str:
+    environment =  os.environ.get('ENVIRONMENT')
+    if environment is None:
+        raise ValueError("ENVIRONMENT environment variable not set")
+    else:
+        return environment
 
-engine = create_engine(DATABASE_URL)
+
+def get_db_url() -> str:
+    db_url = os.environ.get("DATABASE_URL")
+    if db_url is None:
+        raise ValueError("DATABASE_URL environment variable not set")
+    else:
+        return db_url
+
+CURR_DATABASE_URL = get_db_url() if get_environment() == "production" else DEV_DATABASE_URL
+
+
+engine = create_engine(CURR_DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
 
@@ -29,14 +40,13 @@ def get_db() -> Generator:
         db.close()
 
 
-class Base(object):
-    __abstract__ = True
 
+class Base(DeclarativeBase):
     def to_dict(self):
         return {field.name: getattr(self, field.name) for field in self.__table__.c}
 
 
-Base = declarative_base(cls=Base)
+
 
 # https://youtrack.jetbrains.com/issue/PY-58881
 if TYPE_CHECKING:
